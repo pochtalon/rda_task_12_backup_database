4ce99aa6d125b0c42dd0c6403ca4418c76b09b7e
pochtalon <pochtalon1010@gmail.com>
2024-04-19 10:41:44 +0300

    For checking

--- task.sh
+++ task.sh
@@ -15,14 +15,24 @@ if [ -z "$PASSWORD" ]; then
     exit 1
 fi
 
+echo "Variables are present"
+
 # Making full backup for ShopDB data base
 mysqldump -u "$USER" -p "$PASSWORD" --databases ShopDB --result-file=full_backup.sql
+
+echo "First backup is created"
 
 # Restoring new db from backup
 mysql -u "$USER" -p "$PASSWORD" ShopDBReserve < full_backup.sql
 
+echo "First backup is done"
+
 # Making data backup for ShopDB data base
 mysqldump -u "$USER" -p "$PASSWORD" --no-create-info --skip-add-drop-table --databases ShopDB --result-file=data_backup.sql
 
+echo "Second backup is created"
+
 # Moving data from the production database
 mysql -u "$USER" -p "$PASSWORD" ShopDBDevelopment < data_backup.sql
+
+echo "Second backup is done"
