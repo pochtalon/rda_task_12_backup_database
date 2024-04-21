9c14c38e3c1d300428ed9c6365cd68d7d1402f41
pochtalon <pochtalon1010@gmail.com>
2024-04-21 13:38:43 +0300

    Added EOF

--- task.sh
+++ task.sh
@@ -5,3 +5,4 @@ mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBReserve < full-backup.sql
 
 mysqldump -u "$DB_USER" -p"$DB_PASSWORD" ShopDB --result-file=data-backup.sql --no-create-info
 mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBDevelopment < data-backup.sql
+
